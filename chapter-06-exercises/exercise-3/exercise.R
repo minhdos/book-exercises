a495cb6155f487068324ec4727a4fb56f968cf84
Minh tu Huynh <tuhuynh2@uw.edu>
2020-01-17 23:38:05 -0800

    updated exercise files

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -1,18 +1,26 @@
-# Exercise 3: writing and executing functions
+# Exercise 3: writing and executing functtions
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
+add_three <- function(digit) {
+  return(digit + 3)
+}
 
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
-
+ten <- add_three(7)
+print(ten)
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
 # The function should return the equivalent length in meters
-
+imperial_to_metric <- function(feet, inches) {
+  metric_equivalent <- (feet * .3048) + (inches * .0254)
+  return(metric_equivalent)
+}
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
 
+height_in_meters <- imperial_to_metric(5,7)
--- chapter-08-exercises/exercise-1/exercise.R
+++ chapter-08-exercises/exercise-1/exercise.R
@@ -1,29 +1,45 @@
 # Exercise 1: creating and accessing lists
 
 # Create a vector `my_breakfast` of everything you ate for breakfast
+my_breakfast <- list(
+  drink= "smoothie",
+  food = "eggs"
+)
 
 
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
 
+my_lunch <- list(
+  drink = "water", 
+  food = "pad thai"
+)
+
 
 # Create a list `meals` that has contains your breakfast and lunch
 
+meal <- c(my_breakfast, my_lunch)
 
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
+dinner <- list(
+  food = "ramen",
+  drink = "boba"
+)
 
+meal <- c(my_breakfast, my_lunch, dinner)
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
+dinners <- meal$dinner
 
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
 
-
+my_lunch[[5]]
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
-
+early_meals <- c(my_breakfast, my_lunch)
 
 
 ### Challenge ###
